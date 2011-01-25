875e6e8e337ca9d417b49642c7b1540c53c4acad
Conner <cpetzold@gmail.com>
2011-01-25 13:13:14 -0800

    generate markup with jade

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require('express'),
     impact = require('impact');
-      
+  
 var server = express.createServer();
 
 server.configure(function(){
-    server.use(express.methodOverride());
-    server.use(express.bodyDecoder());
-    server.use(server.router);
-    server.use(express.staticProvider(__dirname + '/public'));
+  server.set('views', __dirname + '/views');
+  server.use(express.methodOverride());
+  server.use(express.bodyDecoder());
+  server.use(server.router);
 });
 
-impact.listen(server);
+server.get('/', function(req, res){
+  res.render('index.jade', {
+    locals: { title: 'Heli' }
+  });
+});
+
+var im = impact.listen(server, { root: __dirname + '/public/' });
+server.use(express.staticProvider(im.root));
 
 server.listen(8080);
